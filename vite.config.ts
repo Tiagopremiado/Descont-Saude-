9bf1b246f25a6a2f12ebae02b8fc3437b8707459
Tiagopremiado <pelotaspremiada@gmail.com>
2025-10-31 09:15:47 -0300

    feat: Integrate Vite PWA plugin
    Replace manual service worker registration and manifest handling with the `vite-plugin-pwa` plugin. This simplifies PWA setup and ensures proper service worker generation and manifest integration during the build process.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: "Descont'Saúde PWA",
+        short_name: "Descont'Saúde",
+        description: "A web system for Descont'Saúde to manage clients, dependents, and payments, providing a dedicated portal for clients to access their information and services.",
+        theme_color: '#750721',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: '/favicon.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
 })
